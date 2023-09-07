f2f2b673dfac5ae2d9125ebf937ca3a5c68e5525
Michel <michtene@yahoo.fr>
2023-09-07 20:49:18 +0200

    Project initialization

---
+++ api/src/main/java/com/MyProjects2023/taskflowproject/TaskflowProjectApplication.java
@@ -0,0 +1,13 @@
+package com.MyProjects2023.taskflowproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskflowProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskflowProjectApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/MyProjects2023/taskflowproject/TaskflowProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyProjects2023.taskflowproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskflowProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
